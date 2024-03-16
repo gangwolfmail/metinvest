2fafe9fb344cdc7cdcedd17d8479ff9b6fd2114d
gangwolfmail <129858122+gangwolfmail@users.noreply.github.com>
2024-03-16 21:54:20 +0200

    upd

--- js/dataAboutCompany.js
+++ js/dataAboutCompany.js
@@ -10,7 +10,6 @@ let assetsUKR = {
   'ПРАТ "ПІВНГЗК"': ["м. Кривий Ріг, Першотравневий кар'єр"],
 };
 let webPagesOfAssetsUKR = {
-<<<<<<< HEAD
   "ТОВ «Метінвест Холдинг»": "metinvestholding.com",
   "ТОВ «Метінвест-СМЦ»": "metinvest-smc.com",
   "ТОВ «Метінвест Діджитал»": "metinvest.digital",
@@ -28,19 +27,6 @@ let assetsENG = {
   "Metinvest Very Very Very Digital LLC": ["Kyiv, 12 Laboratorny Lane"],
   '"PIVNGZK"': ["Kriviy Rih"],
 };
-=======
-    "ТОВ «Метінвест Холдинг»": "metinvestholding.com",
-	"ТОВ «Метінвест-СМЦ»": "metinvest-smc.com",
-	"ТОВ «Метінвест Діджитал»": "metinvest.digital",
-	"ТОВ «Метінвест Дуже Дуже Діджитал»": "metinvest.digital"
-let assetsENG = {
-	"Metinvest Holding LLC": ["Kyiv, 12 Laboratorny Lane"],
-	"Metinvest-SMC LLC": ["Kyiv, 8 Kniaziv Ostrozkych street", "Lviv, 317-C Shevchenko street", "Dnipro, 101-A Solonchakova street"],
-	"Metinvest Digital LLC": ["Kyiv, 12 Laboratorny Lane"],
-	"Metinvest Very Very Very Digital LLC": ["Kyiv, 12 Laboratorny Lane"]
-}
-}
->>>>>>> f6f084c62f83835376b4ff7660ebb2b7e9d7858d
 let webPagesOfAssetsENG = {
   "Metinvest Holding LLC": "metinvestholding.com",
   "Metinvest-SMC LLC": "metinvest-smc.com",
