f6f084c62f83835376b4ff7660ebb2b7e9d7858d
gangwolfmail <gangwolf192906@gmail.com>
2024-03-16 17:33:33 +0200

    Update dataAboutCompany.js

--- js/dataAboutCompany.js
+++ js/dataAboutCompany.js
@@ -2,29 +2,24 @@ let assetsUKR = {
 	"ТОВ «Метінвест Холдинг»": ["м. Київ, Лабораторний провулок, 12"],
 	"ТОВ «Метінвест-СМЦ»": ["м. Київ, вул. Князів Острозьких, 8", "м. Львів, вул. Шевченка, 317-С", "м. Дніпро, вул. Солончакова, 101-А"],
 	"ТОВ «Метінвест Діджитал»": ["м. Київ, Лабораторний провулок, 12"],
-	"ТОВ «Метінвест Дуже Дуже Діджитал»": ["м. Київ, Лабораторний провулок, 12"],
-	"ПРАТ «ПІВНГЗК»": ["м. Кривий Ріг, Лабораторний провулок, 12"]
+	"ТОВ «Метінвест Дуже Дуже Діджитал»": ["м. Київ, Лабораторний провулок, 12"]
 }
 let webPagesOfAssetsUKR = {
     "ТОВ «Метінвест Холдинг»": "metinvestholding.com",
 	"ТОВ «Метінвест-СМЦ»": "metinvest-smc.com",
 	"ТОВ «Метінвест Діджитал»": "metinvest.digital",
-	"ТОВ «Метінвест Дуже Дуже Діджитал»": "metinvest.digital",
-	"ПРАТ «ПІВНГЗК»": "metinvestholding.com"
-}
+	"ТОВ «Метінвест Дуже Дуже Діджитал»": "metinvest.digital"
 let assetsENG = {
 	"Metinvest Holding LLC": ["Kyiv, 12 Laboratorny Lane"],
 	"Metinvest-SMC LLC": ["Kyiv, 8 Kniaziv Ostrozkych street", "Lviv, 317-C Shevchenko street", "Dnipro, 101-A Solonchakova street"],
 	"Metinvest Digital LLC": ["Kyiv, 12 Laboratorny Lane"],
-	"Metinvest Very Very Very Digital LLC": ["Kyiv, 12 Laboratorny Lane"],
-	"Metinvest 1111": ["Kyiv, 12 Laboratorny Lane"]
+	"Metinvest Very Very Very Digital LLC": ["Kyiv, 12 Laboratorny Lane"]
 }
 }
 let webPagesOfAssetsENG = {
     "Metinvest Holding LLC": "metinvestholding.com",
 	"Metinvest-SMC LLC": "metinvest-smc.com",
 	"Metinvest Digital LLC": "metinvest.digital",
-	"Metinvest Very Very Very Digital LLC": "metinvest.digital",
-	"Metinvest 1111": "metinvest.digital"
+	"Metinvest Very Very Very Digital LLC": "metinvest.digital"
 }
 
